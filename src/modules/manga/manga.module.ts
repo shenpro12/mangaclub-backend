7d7fabd9d3eda4573cf26e6164095dfe60f04fc8
LeVanDat <104962122+shenpro12@users.noreply.github.com>
2023-07-03 15:06:45 +0700

    Update manga.module.ts

--- src/modules/manga/manga.module.ts
+++ src/modules/manga/manga.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Chapter } from 'src/entities/chapter.entity';
 import { Manga } from 'src/entities/manga.entity';
 import { MangaController } from './manga.controller';
 import { MangaService } from './manga.service';
-import { Chapter } from 'src/entities/chapter.entity';
+
 @Module({
   imports: [TypeOrmModule.forFeature([Manga, Chapter])],
   controllers: [MangaController],
