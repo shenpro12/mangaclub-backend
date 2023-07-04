0e07fa2d9ebfadab82619d30773cda0b9eac836f
LeVanDat <104962122+shenpro12@users.noreply.github.com>
2023-07-04 10:54:09 +0700

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: 'https://mangaclub-beta.vercel.app',
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     credentials: true,
   });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
